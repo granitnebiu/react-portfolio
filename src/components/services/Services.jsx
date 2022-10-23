705fd11f3a05355673c85aac8c4db1263f62782d
granitnebiu <granitnebiu@gmail.com>
2022-10-23 22:52:15 +0200

    service section finished

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,127 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
+
+const firstCard = [
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+];
+
+const secondCard = [
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+];
+const thirdCard = [
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+  {
+    icon: <BiCheck className="service__list-icon" />,
+    text: "Lorem, ipsum dolor sit amet sonsectetuyr elit",
+  },
+];
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services_container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/US Design</h3>
+          </div>
+          <ul className="service__list">
+            {firstCard.map((item, index) => (
+              <li>
+                {item.icon}
+                <p>{item.text}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            {secondCard.map((item, index) => (
+              <li>
+                {item.icon}
+                <p>{item.text}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            {thirdCard.map((item, index) => (
+              <li>
+                {item.icon}
+                <p>{item.text}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
