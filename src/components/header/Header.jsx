4bd394b1d96aa70cdab8b1198d0042597b904e97
granitnebiu <granitnebiu@gmail.com>
2022-10-23 18:00:07 +0200

    header styled and added other components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import CTA from "./CTA";
 import "./header.css";
+import userProfile from "../../assets/svg/user-profile.svg";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Granit Nebiu</h1>
+        <h5 className="text-light">Fullstack developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="user-profile">
+          <img src={userProfile} alt="user-profile" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
